d762bfa4fe57fbadf1d18bb7a3e20a766219d732
sbhatia20 <141480130+sbhatia20@users.noreply.github.com>
2023-08-26 01:51:11 +0530

    Add files via upload
    In this problem, we have to find the shortest subsequence or subarray in an array that is sorted. Using a two pointer approach (first pointer (denoted by i) starting from i=0 and the second (denoted by j) from j=array.size()-1) we calculate the first and last indices of the unsorted array. The length of a subarray is calculated as j-i+1.
    Time complexity is O(NLogN).

---
+++ code.cpp
@@ -0,0 +1,37 @@
+//Approach 1 that solves problem in O(NLogN) time 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+    int findUnsortedSubarray(vector<int>& nums) {
+
+    int n=nums.size();
+    if(n==1)
+    {return 0;}
+    vector<int> original=nums;
+    sort(nums.begin(),nums.end());
+    int ans=0;
+    int i=0;
+    int j=n-1;
+    while(i<j) 
+    {
+    if(original[i]==nums[i] && original[j]==nums[j])
+    {i++;
+    j--;}
+      
+    else if(original[i]==nums[i] && original[j]!=nums[j])
+    {i++;}
+
+    else if(original[i]!=nums[i] && original[j]==nums[j])
+    {j--;}
+     
+    else
+    {ans=j-i+1;
+    break;}}
+    return ans;
+    }
+int main()
+{
+    return 0;
+}
